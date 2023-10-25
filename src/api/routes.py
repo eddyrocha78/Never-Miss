6e9fad14a003f22d9d56fd12e8fd221b2c672237
Benny Oliveira <33961839+bennycarvalho13@users.noreply.github.com>
2023-10-25 19:28:39 +0000

    test api

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+import requests
+import json
 
 api = Blueprint('api', __name__)
 
@@ -15,4 +17,16 @@ def handle_hello():
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
+    return jsonify(response_body), 200
+
+@api.route('/get', methods=['GET'])
+def get_info():
+    res = requests.get('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', headers={
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjJjYjAxZWFiNThjNGRlNzdjOWNhMmY0ZGM4ODQ0NyIsInN1YiI6IjY1Mzk1YmFhZWM0NTUyMDBlYTRkNDMxYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cz3I9EbEUfEny1vJHlbpG7zW_2dSZRBsGCrx6Xy3768"
+})
+    response_body = {
+        "message": res.text
+    }
+    print(res.text)
     return jsonify(response_body), 200
