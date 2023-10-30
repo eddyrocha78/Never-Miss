445c85ada8419c707e871272e1c2cb7f7da4e667
Benny Oliveira <33961839+bennycarvalho13@users.noreply.github.com>
2023-10-30 20:13:41 +0000

    updated bootstrap

---
+++ src/front/js/pages/movieDetails.js
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+
+export const Details = () => {
+	const { store, actions } = useContext(Context);
+
+	const [info, setInfo ] = useState([]);
+	
+	const getInfo = () => 
+	{
+		const options = {
+			method: 'GET',
+			headers: {
+			  accept: 'application/json',
+			  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjJjYjAxZWFiNThjNGRlNzdjOWNhMmY0ZGM4ODQ0NyIsInN1YiI6IjY1Mzk1YmFhZWM0NTUyMDBlYTRkNDMxYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cz3I9EbEUfEny1vJHlbpG7zW_2dSZRBsGCrx6Xy3768'
+			}
+		  };
+		  
+		  fetch('https://api.themoviedb.org/3/movie/299054?language=en-US', options)
+			.then(response => response.json())
+			.then(response => {console.log(response); setInfo(response)})
+			.catch(err => console.error(err));
+	}
+
+	return (
+		<div className="text-center mt-5">
+			<h1>{info.title}</h1>
+			<button className="btn btn-primary" onClick={getInfo}> Get Info</button>
+			<p>
+			{info.overview}
+			</p>
+			<div className="alert alert-info">
+				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+			</div>
+			<p>
+				This boilerplate comes with lots of documentation:{" "}
+				<a href="https://start.4geeksacademy.com/starters/react-flask">
+					Read documentation
+				</a>
+			</p>
+		</div>
+	);
+};
