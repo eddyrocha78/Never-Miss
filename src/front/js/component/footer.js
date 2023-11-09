ee9e4723de2fa00e36a87780f6255ed2f0722311
ZionSpardas <135459718+ZionSpardas@users.noreply.github.com>
2023-11-09 23:01:34 +0000

    Footer Finished, fixed the typo and it collapses when in a small screen

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,18 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
 export const Footer = () => (
-	<footer style={{ backgroundColor: "rgba(37, 53, 37, 1)" }}  className="pt-3 pb-3 text-white">
-		<div className="row mx-4">
-			<p className="text-center">Europoe <a href="https://4geeks.com">4 Geeks Academy</a> final Project</p>
-		</div>
-		<div style={{ backgroundColor: "rgba(16, 17, 18, 1)" }} className="row mx-4 py-2 rounded">
-			<p className="text-center">Created by:</p>
-			<ul   className="list-group list-group-horizontal text-center border-0 me-4">
-				<li style={{ backgroundColor: "rgba(16, 17, 18, 1)" }} className="list-group-item col border-0"><i className="fa-brands fa-github fa-xl me-2 text-white"></i><a href="https://github.com/bennycarvalho13">Benny Carvalho</a></li>
-				<li style={{ backgroundColor: "rgba(16, 17, 18, 1)" }} className="list-group-item col border-0"><i className="fa-brands fa-github fa-xl me-2 text-white"></i><a href="https://github.com/ZionSpardas">Daniel Barbosa</a></li>
-				<li style={{ backgroundColor: "rgba(16, 17, 18, 1)" }} className="list-group-item col border-0"><i className="fa-brands fa-github fa-xl me-2 text-white"></i><a href="https://github.com/eddyrocha78">Eduardo Rocha</a></li>
-				<li style={{ backgroundColor: "rgba(16, 17, 18, 1)" }} className="list-group-item col border-0"><i className="fa-brands fa-github fa-xl me-2 text-white"></i><a href="https://github.com/drbig30">David R</a></li>
-			</ul>
-		</div>
-	</footer>
+  <footer className="text-white">
+    <div style={{ backgroundColor: "rgba(37, 53, 37, 1)" }} className="container-fluid pt-2">
+      <div className="row">
+        <p className="text-center">Europe <a href="https://4geeks.com">4 Geeks Academy</a> final Project</p>
+      </div>
+      <div className="row py-2 mx-2 rounded">
+        <p className="text-center">Created by:</p>
+        <ul className=" list-group list-group-horizontal text-center flex-wrap border-0 me-4 navbar-collapse">
+          <li className="transp list-group-item col-12 col-sm-6 col-md-3 border-0">
+            <i className="fab fa-github fa-xl me-2 text-white"></i><a href="https://github.com/bennycarvalho13">Benny Carvalho</a>
+          </li>
+          <li className="transp list-group-item col-12 col-sm-6 col-md-3 border-0">
+            <i className="fab fa-github fa-xl me-2 text-white"></i><a href="https://github.com/ZionSpardas">Daniel Barbosa</a>
+          </li>
+          <li className="transp list-group-item col-12 col-sm-6 col-md-3 border-0">
+            <i className="fab fa-github fa-xl me-2 text-white"></i><a href="https://github.com/eddyrocha78">Eduardo Rocha</a>
+          </li>
+          <li className="transp list-group-item col-12 col-sm-6 col-md-3 border-0">
+            <i className="fab fa-github fa-xl me-2 text-white"></i><a href="https://github.com/drbig30">David Roger</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 );
