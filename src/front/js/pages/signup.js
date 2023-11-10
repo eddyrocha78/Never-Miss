91bb17911a5bb90738b69da06c2544e9080ef69f
ZionSpardas <135459718+ZionSpardas@users.noreply.github.com>
2023-11-10 19:07:18 +0000

    signup fixed

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import "../../styles/signup.css";
+
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 
@@ -14,70 +14,95 @@ export const Signup = () => {
 		email: '',
 		password: '',
 		confirmPassword: ''
-	  });
-	
-	  const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormData({...formData, [name]: value});
-	  };
+	});
 
-	  const handleSubmit = async (e) => {
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
 		if (formData.password !== formData.confirmPassword) {
 			setError("Passwords don't match");
-		  return;
+			return;
 		}
-		
+
 		setError(null);
 		let isSignedUp = await actions.signup(formData);
-		if(isSignedUp) {
+		if (isSignedUp) {
 			navigate("/login");
 		}
-	  };
-	
-	
-	  return (
-		<div className="signup-wrapper">
-			<div className="signup-form col-md-6 offset-md-3 ">
-				<h1>Never Miss</h1>
-				<p>another movie or episode ever again!</p>
-				<p>{error && {error}}</p>
+	};
 
-				<form onSubmit={handleSubmit} className="row m-3">
-					<div className="row">
-						<div className="col-md-6 mb-3">
-							<input type="text" name="firstName" placeholder="First Name" onChange={handleChange} required/>
-						</div>
-						<div className="col-md-6 mb-3">
-							<input type="text" name="lastName" placeholder="Last Name" onChange={handleChange} required/>
-						</div>
-					</div>
 
-					<div className="row">
-						<div className="col-md-12 mb-3">
-							<input type="email" name="email" placeholder="Email" onChange={handleChange} required/>
-						</div>
-						<div className="col-md-12 mb-3">
-							<input type="password" name="password" placeholder="Password" onChange={handleChange} required/>
-						</div>
-						<div className="col-md-12 mb-3">
-							<input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange} required/>
-						</div>
-					</div>
-					<div className="row justify-content-center">
-						<div className="form-check col-auto mb-3">
-							<input className="form-check-input" type="checkbox" checked={isChecked} onChange={() => setIsChecked(!isChecked)}/>
-							<label className="form-check-label" htmlFor="flexCheckDefault">
-							I've read and agree with the <a className="termsandconditions">Terms and Conditions</a>
-							</label>
-						</div>
-					</div>
+	return (
 
-					<div className="row justify-content-center">
-						<button disabled={!isChecked} className="col-auto mb-3" type="submit">Sign Up</button>
+		<div className="container-fluid" >
+			<div className=" text-white rounded p-4 my-5 col-4 mx-auto" style={{ backgroundColor: "rgba(37, 53, 37, 1)" }} >
+				<div className="row">
+					<div className="text-center">
+						<h1 className=" display-4">Never Miss</h1>
+						<p className="fs-3 text-white-50">another movie or episode ever again!</p>
+						<p>{error && { error }}</p>
 					</div>
-				</form>
+					{store.token && store.token !== null && store.token !== "" ? (
+						<div className="row text-center">
+							<h5 className="text-center">You are already an esteemed member</h5>
+						</div>
+					) : (
+						<form onSubmit={handleSubmit} className="row m-3">
+							<div className="row d-flex text-center">
+								<div className="col-md-6 mb-3">
+									<label for="firstname" class="form-label">First Name:</label>
+									<input id="firstname" className="form-control" type="text" name="firstName" placeholder="First Name" onChange={handleChange} required />
+								</div>
+								<div className="col-md-6 mb-3">
+									<label for="lastname" class="form-label">Last Name:</label>
+									<input id="lastname" className="form-control" type="text" name="lastName" placeholder="Last Name" onChange={handleChange} required />
+								</div>
+							</div>
+
+							<div className="row ">
+								<div className="col-md-6 text-center mb-3">
+									<label for="email" class="form-label">Email :</label>
+								</div>
+								<div className="col-md-6  mb-3">
+									<input id="email" className="form-control" type="email" name="email" placeholder="Email" onChange={handleChange} required />
+								</div>
+							</div>
+							<div className="row">
+								<div className="col-md-6 text-center mb-3">
+									<label for="password" class="form-label">Password :</label>
+								</div>
+								<div className="col-md-6 mb-3">
+									<input id="password" className="form-control" type="password" name="password" placeholder="Password" onChange={handleChange} required />
+								</div>
+							</div>
+							<div className="row">
+								<div className="col-md-6 mb-3 text-center mx-auto">
+									<label for="passwordcheck" class="form-label">Confirm Password :</label>
+								</div>
+								<div className="col-md-6 mb-3 mx-auto">
+									<input id="passwordcheck" className="form-control" type="password" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange} required />
+								</div>
+							</div>
+							<div className="row justify-content-center">
+								<div className="form-check col-auto mb-3">
+									<input className="form-check-input" type="checkbox" checked={isChecked} onChange={() => setIsChecked(!isChecked)} />
+									<label className="form-check-label" htmlFor="flexCheckDefault">
+										I've read and agree with the <a href="#">Terms and Conditions</a>
+									</label>
+								</div>
+							</div>
+
+							<div className="row justify-content-center">
+								<button disabled={!isChecked} className="col-3 btn btn-success" type="submit">Sign Up</button>
+							</div>
+						</form>
+					)}
+				</div>
 			</div>
 		</div>
-	  );
-	}
+	);
+}
