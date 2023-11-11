aae2ee95c2ad81c8475371b9de62ee9cbf50435d
ZionSpardas <135459718+ZionSpardas@users.noreply.github.com>
2023-11-11 13:43:36 +0000

    fixing

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -6,61 +6,61 @@ const getState = ({ getStore, getActions, setStore }) => {
 			token: null,
 			message: null,
 			user: []
-			
+
 		},
 		actions: {
 			syncTokenFromSessionStore: () => {
 				const token = sessionStorage.getItem("token");
 				console.log("Application loaded, synching session storage token");
-				if(token && token !="" && token !=undefined) setStore({token: token});
+				if (token && token != "" && token != undefined) setStore({ token: token });
 			},
 
 			logout: () => {
 				sessionStorage.removeItem("token");
 				console.log("Loging out");
-				setStore({token: null});
+				setStore({ token: null });
 			},
 
 			login: async (email, password) => {
 				console.log(email, password);
-				try{
-				// Creating opts for the fetch
-				const opts = {
-					method: "POST",
-					headers: {"Content-Type": "application/json"},
-					body: JSON.stringify({
-						email: email,
-						password: password
-					})
-				};
+				try {
+					// Creating opts for the fetch
+					const opts = {
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							email: email,
+							password: password
+						})
+					};
 
-				
-				const resp = await fetch("https://expert-space-sniffle-rjqgqg6rvj4cxgg-3001.app.github.dev/api/login", opts)
 
-				console.log(resp)
-					if(!resp.ok){
+					const resp = await fetch(process.env.BACKEND_URL + "api/login", opts)
+
+					console.log(resp)
+					if (!resp.ok) {
 						alert("Error detected");
 						return false;
 					}
 
-				const data = await resp.json();
-				console.log("Backend data", data);
+					const data = await resp.json();
+					console.log("Backend data", data);
 
-				sessionStorage.setItem("token", data.access_token);
-				setStore({token: data.access_token});	
-				return true;
+					sessionStorage.setItem("token", data.access_token);
+					setStore({ token: data.access_token });
+					return true;
 
 				}
-				catch(error){
+				catch (error) {
 					console.error("Error detected on login")
 				}
-				
+
 			},
 
 			signup: async (signUpData) => {
 				console.log("DATA!!!!");
 				console.log(signUpData);
-				
+
 				// Creating opts for the fetch
 				const opts = {
 					method: "POST",
@@ -69,12 +69,12 @@ const getState = ({ getStore, getActions, setStore }) => {
 					},
 					body: JSON.stringify(signUpData)
 				};
-				
-				try{	
-					const resp = await fetch("https://expert-space-sniffle-rjqgqg6rvj4cxgg-3001.app.github.devapi/signup", opts);
-					
+
+				try {
+					const resp = await fetch(process.env.BACKEND_URL + "api/signup", opts)
+
 					console.log(resp)
-					if(!resp.ok){
+					if (!resp.ok) {
 						alert("Error detected");
 						return false;
 					}
@@ -84,24 +84,24 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 					return true;
 				}
-				catch(error){
+				catch (error) {
 					console.error("Error detected on login")
 				}
 
 			},
 
 
-			forgotPassword: async (modalData) =>{
+			forgotPassword: async (modalData) => {
 				console.log(modalData);
 				//insert 3rd party API to send email to user with backend stored email
-				try{	
+				try {
 					const resp = await fetch("https://expert-space-sniffle-rjqgqg6rvj4cxgg-3001.app.github.devapi/users");
 					const data = await resp.json()
 					console.log(data)
 
 					return true;
 				}
-				catch(error){
+				catch (error) {
 					console.error("Error detected on sending user password")
 				}
 			},
@@ -114,18 +114,18 @@ const getState = ({ getStore, getActions, setStore }) => {
 						"Authorization": "Bearer " + store.token
 					}
 				}
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "api/hello", opts)
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
-			
+
 		}
 	};
 };
