e62eb098f51003b346e3e14bcb8f2c7246a3219a
Eduardo Rocha <134506270+eddyrocha78@users.noreply.github.com>
2023-11-12 16:09:54 +0000

    userspace done

--- src/front/js/component/CardFavorite.js
+++ src/front/js/component/CardFavorite.js
@@ -6,13 +6,13 @@ export const CardFavorite = ({ imgpath, title, release_date, runtime, genres }) 
 
   return (
     <div className="container">
-      <div className="card mb-3 me-1 container-fluid" style={{ height: "100px", width: "auto" }}>
+      <div className="card mb-3 me-1 container-fluid" style={{ backgroundColor: "rgba(82, 117, 82, 1)", height: "100px", width: "auto" }}>
         <div className="row g-0">
-          <div className="col-md-4">
+          <div className="col mt-2">
             <img src={url} className="img-fluid rounded-start" alt={title}/>
           </div>
-          <div className="col-md-8 text-start">
-            <div className="card-body">
+          <div className="col-md-10">
+            <div className="card-body text-start ps-2 mt-0 pt-0">
               <h5 className="card-title">{title}</h5>
               <p className="card-text"><small>Release Date {release_date} | Duration {runtime} min | Genre {genres}</small></p>
             </div>
