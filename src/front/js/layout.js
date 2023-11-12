7e0db86f3855911b839fedd88e94607458df728d
Eduardo Rocha <134506270+eddyrocha78@users.noreply.github.com>
2023-11-12 23:13:41 +0000

    userprofile layout done

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Signup } from "./pages/signup";
 import { Login } from "./pages/login";
+import { UserProfile2 } from "./pages/userProfile2";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import { Details } from "./pages/movieDetails";
@@ -34,6 +35,7 @@ const Layout = () => {
             <Route element={<Demo />} path="/demo" />
             <Route exact element={<Login />} path="/login" />
             <Route exact element={<Signup />} path="/signup" />
+            <Route exact element={<UserProfile2 />} path="/userProfile2" />
             <Route element={<Results />} path="/search/:keyword/:page" />
             <Route element={<Details />} path=":type/details/:theid" />
             <Route element={<Single />} path="/single/:theid" />
