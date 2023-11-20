a0ce7691df93d52e486c10c6333369598d0bb36f
ZionSpardas <135459718+ZionSpardas@users.noreply.github.com>
2023-11-20 07:50:45 +0000

    cant make it work codespace dosent like me

--- src/front/js/component/CardFavorite.js
+++ src/front/js/component/CardFavorite.js
@@ -3,21 +3,21 @@ import React from "react";
 
 
 
-export const CardFavorite = ({ imgpath, title, release_date, runtime, genres }) => {
+export const CardFavorite = ({title,}) => {
   const url = 'https://image.tmdb.org/t/p/w100_and_h150_bestv2' + imgpath;
   
 
   return (
     <div className="container">
-      <div className="card mb-3 me-1 container-fluid" style={{ backgroundColor: "rgba(82, 117, 82, 1)", height: "100px", width: "auto" }}>
-        <div className="row g-0">
+      <div className="mb-3 me-1 container-fluid" style={{ backgroundColor: "rgba(82, 117, 82, 1)", height: "100px", width: "auto" }}>
+        <div className="row ">
           <div className="col mt-2">
             <img src={url} className="img-fluid rounded-start" alt={title}/>
           </div>
           <div className="col-md-10">
-            <div className="card-body text-start ps-2 mt-0 pt-0">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text"><small>Release Date {/*info.release_date*/} | Duration {/*info.runtime*/} min | Genre {/*info.genres.name*/}</small></p>
+            <div className="text-start ps-2 mt-0 pt-0">
+              <h5 className="">{title}</h5>
+    
             </div>
           </div>
         </div>
--- src/front/js/pages/userspace.js
+++ src/front/js/pages/userspace.js
@@ -4,83 +4,72 @@ import { Context } from "../store/appContext";
 import { CardFavorite } from "../component/CardFavorite";
 import { Link, useParams, useNavigate } from "react-router-dom";
 
+
 export const UserSpace = () => {
   const { store, actions } = useContext(Context);
   const [info, setInfo] = useState([]);
 
-	useEffect(() => {
-		if(store.token && store.token !="" && store.token !=null) actions.getMessage();
-	}, [store.token])
 
-	let sessiontoken = sessionStorage.getItem("token")
-  
+  let sessiontoken = sessionStorage.getItem("token")
+
   const navigate = useNavigate();
 
-  const getInfo = () => {
-		const options = {
-			method: 'GET',
-			headers: {
-				accept: 'application/json',
-				Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjJjYjAxZWFiNThjNGRlNzdjOWNhMmY0ZGM4ODQ0NyIsInN1YiI6IjY1Mzk1YmFhZWM0NTUyMDBlYTRkNDMxYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cz3I9EbEUfEny1vJHlbpG7zW_2dSZRBsGCrx6Xy3768'
-			}
-		};
+  useEffect(() => {
+    actions.getFavorites(store.userId);
 
-		fetch(`https://api.themoviedb.org/3/${mediatype}/${mediaId}?language=en-US`, options)
-			.then(response => response.json())
-			.then(response => {console.log(response); setInfo(response) })
-			.catch(err => console.error(err));
-	};
-
+  }, [store.userId]);
+  console.log("favoritos", store.userFavorites);
   return (
     <div className="container-fluid">
       <div className="row text-center my-5">
         <h1 className="text-white display-1 fw-bold">{store.userName} {store.userLastName} Space</h1>
       </div>
-      
+
       <div className="content-wrapper container">
-        <div  className="row justify-content-center">
+        <div className="row justify-content-center">
           <div className="currently-watching rounded col-md-5 pe-2 mb-2" style={{ color: "rgba(225, 225, 225, 1)", backgroundColor: "rgba(37, 53, 37, 1)" }}>
             <div className="header container-fluid py-2 d-inline-flex align-items-center">
               <i className="fa-solid fa-eye fa-lg ms-1 me-3 text-success"></i>
               <h4>Currently Watching</h4>
+              {store.userfavorites.map((key, index) => <CardFavorite title={index.title} />)}
             </div>
             <div className="Movies-wrapper">
-              <CardFavorite />
+             
             </div>
           </div>
-        
-        
-          <div className="col-md-5">  
-            <div className="plan-to-watch row ms-1 rounded mb-2" style={{ color: "rgba(225, 225, 225, 1)", backgroundColor: "rgba(37, 53, 37, 1)" }}>
-              <div className="col">
-                <div className="header d-inline-flex py-2 align-items-center">
-                  <i className="fa-solid fa-eye fa-lg ms-1 me-3 text-primary"></i>
-                  <h4>Plan to Watch</h4>
-                </div>
+
+
+            <div className="col-md-5">
+              <div className="plan-to-watch row ms-1 rounded mb-2" style={{ color: "rgba(225, 225, 225, 1)", backgroundColor: "rgba(37, 53, 37, 1)" }}>
+                <div className="col">
+                  <div className="header d-inline-flex py-2 align-items-center">
+                    <i className="fa-solid fa-eye fa-lg ms-1 me-3 text-primary"></i>
+                    <h4>Plan to Watch</h4>
+                  </div>
                   <div style={{ overflowX: "scroll", backgroundColor: "black" }} className="container-fluid plan-to-watch rounded mb-1">
                     {/* plan to watch list*/}
                     1. movie<br></br>
                     2. Serie
                   </div>
-              </div>
-            </div>
-          
-            <div className="completed row ms-1 rounded mb-2" style={{ color: "rgba(225, 225, 225, 1)", backgroundColor: "rgba(37, 53, 37, 1)" }}>
-              <div className="col">
-                <div className="header d-inline-flex align-items-center py-2 ">
-                  <i className="fa-solid fa-eye fa-lg ms-1 me-3 text-secondary"></i>
-                  <h4>Completed</h4>
-                </div>
-                <div style={{ overflowX: "scroll", backgroundColor: "black" }} className="container-fluid completed rounded d-flex flex-row mb-1">
-                  {/* plan to watch list*/}
-                  1. movie<br></br>
-                  2. Serie
                 </div>
               </div>
+
+              <div className="completed row ms-1 rounded mb-2" style={{ color: "rgba(225, 225, 225, 1)", backgroundColor: "rgba(37, 53, 37, 1)" }}>
+                <div className="col">
+                  <div className="header d-inline-flex align-items-center py-2 ">
+                    <i className="fa-solid fa-eye fa-lg ms-1 me-3 text-secondary"></i>
+                    <h4>Completed</h4>
+                  </div>
+                  <div style={{ overflowX: "scroll", backgroundColor: "black" }} className="container-fluid completed rounded d-flex flex-row mb-1">
+                    {/* plan to watch list*/}
+                    1. movie<br></br>
+                    2. Serie
+                  </div>
+                </div>
+              </div>
             </div>
-          </div> 
+          </div>
         </div>
       </div>
-    </div>
-  );
+      );
 };
