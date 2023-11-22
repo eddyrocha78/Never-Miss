e375ae8d91f8c0f86bcb8a8bc0c4706e6d522ae4
Benny Oliveira <bennycarvalho13@gmail.com>
2023-11-22 12:58:31 +0000

    polish

--- src/front/js/component/CardFavorite.js
+++ src/front/js/component/CardFavorite.js
@@ -5,41 +5,72 @@ import { Context } from "../store/appContext";
 
 
 
-export const CardFavorite = ({ title, imgpath, runtime }) => {
+export const CardFavorite = ({ title, imgpath, runtime, theid, type, userId, status }) => {
   const { store, actions } = useContext(Context);
 
-const removeFavorite = () => {
-  if (store.token && store.token != "" && store.token != null) {
-    actions.deleteFavorite(store.userId, params.theid, params.type)
-    window.location.reload();
+  const removeFavorite = () => {
+    if (store.token && store.token != "" && store.token != null) {
+      actions.deleteFavorite(userId, theid, type);
+      window.location.reload();
+    }
   }
-}
 
-const updateFavorite = (status) => {
-  if (store.token && store.token != "" && store.token != null) {
-    actions.updateFavorite(store.userId, params.theid, params.type, status)
-    window.location.reload();
+  const updateFavorite = (status) => {
+    if (store.token && store.token != "" && store.token != null) {
+      actions.updateFavorite(userId, theid, type, status);
+      window.location.reload();
+    }
   }
-}
 
   const imageUrl = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2' + imgpath;
 
   return (
     <div style={{ background: "rgba(82, 117, 82, 1)" }} className="favorite-card m-2 p-1 d-flex rounded">
-      <div className="col-3">
-        <img src={imageUrl} className="img-fluid sizer" alt={title} />
+      <div className="col-3 d-flex flex-column">
+      <img src={imageUrl} className="img-fluid sizer rounded" alt={title} />
       </div>
       <div className="col-9 d-flex flex-column">
         <h2 className="title">{title}</h2>
         <p className="runtime">{runtime} minutes</p>
-        <button className="btn dropdown-toggle align-text-bottom align-self-end mt-auto" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <i className="fa-solid fa-eye fa-lg text-success-emphasis"></i>
-        </button>
-        <ul className="dropdown-menu dropdown-menu-end dropdown-menu-dark">
-          <li><button className="dropdown-item " type="button" onClick={() => { removeFavorite() }}><i className="fa-solid fa-trash ms-1 fa-lg me-3 text-danger"></i>Remove</button></li>
-          <li><button className="dropdown-item " type="button" onClick={() => { updateFavorite("watched") }}><i className="fa-solid fa-eye fa-lg me-3 text-secondary"></i>Change to Watched</button></li>
-          <li><button className="dropdown-item " type="button" onClick={() => { updateFavorite("planToWatch") }}><i className="fa-solid fa-eye fa-lg me-3 text-primary"></i>Change to Plan to Watch</button></li>
-        </ul>
+        {status == "watching" ?
+          <div className="d-flex flex-column mt-4">
+            <button className="btn btn-dark dropdown-toggle align-text-bottom align-self-end mt-auto" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <i className="fa-solid fa-eye fa-lg me-3 text-success"></i>
+            </button>
+            <ul className="dropdown-menu dropdown-menu-end dropdown-menu-dark">
+              <li><button className="dropdown-item px-4 fs-3" type="button" onClick={() => { removeFavorite() }}><i className="fa-solid fa-trash ms-1 fa-lg me-3 text-danger"></i>Remove</button></li>
+              <li><button className="dropdown-item px-4 fs-3" type="button" onClick={() => { updateFavorite("watched") }}><i className="fa-solid fa-eye fa-lg me-3 text-secondary"></i>Change to Watched</button></li>
+              <li><button className="dropdown-item px-4 fs-3" type="button" onClick={() => { updateFavorite("planToWatch") }}><i className="fa-solid fa-eye fa-lg me-3 text-primary"></i>Change to Plan to Watch</button></li>
+            </ul>
+          </div>
+          :
+          status == "watched" ?
+            <div className="d-flex flex-column mt-4">
+              <button className="btn btn-dark dropdown-toggle align-text-bottom align-self-end mt-auto" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i className="fa-solid fa-eye fa-lg me-3 text-secondary"></i>
+              </button>
+              <ul className="dropdown-menu dropdown-menu-end dropdown-menu-dark">
+                <li><button className="dropdown-item px-4 fs-3" type="button" onClick={() => { removeFavorite() }}><i className="fa-solid fa-trash ms-1 fa-lg me-3 text-danger"></i>Remove</button></li>
+                <li><button className="dropdown-item px-4 fs-3" type="button" onClick={() => { updateFavorite("watching") }}><i className="fa-solid fa-eye fa-lg me-3 text-success"></i>Change to Watching</button></li>
+                <li><button className="dropdown-item px-4 fs-3" type="button" onClick={() => { updateFavorite("planToWatch") }}><i className="fa-solid fa-eye fa-lg me-3 text-primary"></i>Change to Plan to Watch</button></li>
+              </ul>
+            </div>
+            :
+            status == "planToWatch" ?
+              <div className="d-flex flex-column mt-4">
+                <button className="btn btn-dark dropdown-toggle align-text-bottom align-self-end mt-auto" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i className="fa-solid fa-eye fa-lg me-3 text-primary"></i>
+                </button>
+                <ul className="dropdown-menu dropdown-menu-end dropdown-menu-dark">
+                  <li><button className="dropdown-item px-4 fs-3" type="button" onClick={() => { removeFavorite() }}><i className="fa-solid fa-trash ms-1 fa-lg me-3 text-danger"></i>Remove</button></li>
+                  <li><button className="dropdown-item px-4 fs-3" type="button" onClick={() => { updateFavorite("watching") }}><i className="fa-solid fa-eye fa-lg me-3 text-success"></i>Change to Watching</button></li>
+                  <li><button className="dropdown-item px-4 fs-3" type="button" onClick={() => { updateFavorite("watched") }}><i className="fa-solid fa-eye fa-lg me-3 text-secondary"></i>Change to Watched</button></li>
+                </ul>
+              </div>
+              :
+              <div>
+
+              </div>}
       </div>
     </div>
   );
--- src/front/js/pages/userspace.js
+++ src/front/js/pages/userspace.js
@@ -36,11 +36,11 @@ export const UserSpace = () => {
           <div className="currently-watching rounded col-md-5 pe-2 mb-2" style={{ color: "rgba(225, 225, 225, 1)", backgroundColor: "rgba(37, 53, 37, 1)" }}>
             <div className="header container-fluid py-2 d-inline-flex align-items-center">
               <i className="fa-solid fa-eye fa-lg ms-1 me-3 text-success"></i>
-              <h4>Currently Watching</h4>
+              <h4>Watching</h4>
             </div>
             <div className="row">
               <div className="col">
-                <h5 className="mx-3">Movies</h5>
+                <h5 className="bg bg-dark rounded p-1 mx-1">Movies</h5>
               </div>
             </div>
             <div className="row">
@@ -54,6 +54,10 @@ export const UserSpace = () => {
                           title={favorite.title}
                           imgpath={favorite.poster}
                           runtime={favorite.runtime}
+                          userId={store.userId}
+                          type={"movie"}
+                          theid={favorite.movieId}
+                          status={"watching"}
                         />
                       </Link>
                     ) : null
@@ -62,7 +66,7 @@ export const UserSpace = () => {
             </div>
             <div className="row">
               <div className="col">
-                <h5 className="mx-3">Series</h5>
+                <h5 className="bg bg-dark rounded p-1 mx-1">Series</h5>
               </div>
             </div>
             <div className="row">
@@ -70,12 +74,16 @@ export const UserSpace = () => {
                 {store.userFavorites.map((favorite, index) => (
                   (favorite.status === "watching" && favorite.seriesId) ?
                     (
-                      <Link to={"/tv/details/" + favorite.seriesId}>
+                      <Link key={index} to={"/tv/details/" + favorite.seriesId}>
                         <CardFavorite
                           key={index}
                           title={favorite.title}
                           imgpath={favorite.poster}
                           runtime={favorite.runtime}
+                          userId={store.userId}
+                          type={"tv"}
+                          theid={favorite.seriesId}
+                          status={"watching"}
                         />
                       </Link>
                     ) : null
@@ -85,7 +93,7 @@ export const UserSpace = () => {
           </div>
 
           <div className="col-md-5">
-            <div className="plan-to-watch row ms-1 rounded mb-2" style={{ color: "rgba(225, 225, 225, 1)", backgroundColor: "rgba(37, 53, 37, 1)" }}>
+            <div className="plan-to-watch row ms-1 rounded mb-2" style={{ color: "rgba(225, 225, 225, 1)", backgroundColor: "rgba(26, 59, 86, 1)" }}>
               <div className="col">
                 <div className="header d-inline-flex py-2 align-items-center">
                   <i className="fa-solid fa-eye fa-lg ms-1 me-3 text-primary"></i>
@@ -93,19 +101,23 @@ export const UserSpace = () => {
                 </div>
                 <div className="row">
                   <div className="col">
-                    <h5 className="mx-1">Movies</h5>
+                    <h5 className="bg bg-dark rounded p-1 mx-1">Movies</h5>
                   </div>
                 </div>
                 <div className="row">
                   <div className="col">
                     {store.userFavorites.map((favorite, index) => (
                       (favorite.status === "planToWatch" && favorite.movieId) ?
-                        <Link to={"/movie/details/" + favorite.movieId}>
+                        <Link key={index} to={"/movie/details/" + favorite.movieId}>
                           <CardFavorite
                             key={index}
                             title={favorite.title}
                             imgpath={favorite.poster}
                             runtime={favorite.runtime}
+                            userId={store.userId}
+                            type={"movie"}
+                            theid={favorite.movieId}
+                            status={"planToWatch"}
                           />
                         </Link>
                         : null
@@ -114,19 +126,24 @@ export const UserSpace = () => {
                 </div>
                 <div className="row">
                   <div className="col">
-                    <h5 className="mx-1">Series</h5>
+                    <h5 className="bg bg-dark rounded p-1 mx-1">Series</h5>
                   </div>
                 </div>
                 <div className="row">
                   <div className="col">
                     {store.userFavorites.map((favorite, index) => (
                       (favorite.status === "planToWatch" && favorite.seriesId) ?
-                        <Link to={"/tv/details/" + favorite.seriesId}>
+                        <Link key={index} to={"/tv/details/" + favorite.seriesId}>
                           <CardFavorite
                             key={index}
                             title={favorite.title}
                             imgpath={favorite.poster}
                             runtime={favorite.runtime}
+                            userId={store.userId}
+                            type={"tv"}
+                            theid={favorite.seriesId}
+                            status={"planToWatch"}
+
                           />
                         </Link>
                         : null
@@ -136,27 +153,31 @@ export const UserSpace = () => {
               </div>
             </div>
 
-            <div className="completed row ms-1 rounded mb-2" style={{ color: "rgba(225, 225, 225, 1)", backgroundColor: "rgba(37, 53, 37, 1)" }}>
+            <div className="completed row ms-1 rounded mb-2" style={{ color: "rgba(225, 225, 225, 1)", backgroundColor: "rgba(52, 59, 53, 1)" }}>
               <div className="col">
                 <div className="header d-inline-flex align-items-center py-2 ">
                   <i className="fa-solid fa-eye fa-lg ms-1 me-3 text-secondary"></i>
-                  <h4>Completed</h4>
+                  <h4>Watched</h4>
                 </div>
                 <div className="row">
                   <div className="col">
-                    <h5 className="mx-1">Movies</h5>
+                    <h5 className="bg bg-dark rounded p-1 mx-1">Movies</h5>
                   </div>
                 </div>
                 <div className="row">
                   <div className="col">
                     {store.userFavorites.map((favorite, index) => (
                       (favorite.status === "watched" && favorite.movieId) ?
-                        <Link to={"/movie/details/" + favorite.movieId}>
+                        <Link key={index} to={"/movie/details/" + favorite.movieId}>
                           <CardFavorite
                             key={index}
                             title={favorite.title}
                             imgpath={favorite.poster}
                             runtime={favorite.runtime}
+                            userId={store.userId}
+                            type={"movie"}
+                            theid={favorite.movieId}
+                            status={"watched"}
                           />
                         </Link>
                         : null
@@ -165,19 +186,23 @@ export const UserSpace = () => {
                 </div>
                 <div className="row">
                   <div className="col">
-                    <h5 className="mx-1">Series</h5>
+                    <h5 className= "bg bg-dark rounded p-1 mx-1">Series</h5>
                   </div>
                 </div>
                 <div className="row">
                   <div className="col">
                     {store.userFavorites.map((favorite, index) => (
                       (favorite.status === "watched" && favorite.seriesId) ?
-                        <Link to={"/tv/details/" + favorite.seriesId}>
+                        <Link key={index} to={"/tv/details/" + favorite.seriesId}>
                           <CardFavorite
                             key={index}
                             title={favorite.title}
                             imgpath={favorite.poster}
                             runtime={favorite.runtime}
+                            userId={store.userId}
+                            type={"tv"}
+                            theid={favorite.seriesId}
+                            status={"watched"}
                           />
                         </Link>
                         : null
